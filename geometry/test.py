790d67c1bef60033655dd7386456518595a4feff
燕郊码怪 <2363104914@qq.com>
2022-02-13 19:04:05 +0800

    new: 常用几何学处理

---
+++ geometry/test.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def _polygon_area(self, poly):
+    """
+    计算多边形的面积
+    原理：https://zhuanlan.zhihu.com/p/110025234
+    :param poly:[[0,4],[4,4],[4,0],[0,0]] # 保证输入的poly点是按顺时针或逆时针连续传入的
+    """
+    edge = [(poly[1][0] - poly[0][0]) * (poly[1][1] + poly[0][1]),
+            (poly[2][0] - poly[1][0]) * (poly[2][1] + poly[1][1]),
+            (poly[3][0] - poly[2][0]) * (poly[3][1] + poly[2][1]),
+            (poly[0][0] - poly[3][0]) * (poly[0][1] + poly[3][1])]
+    return int(np.sum(edge) / 2.)
